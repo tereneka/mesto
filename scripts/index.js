9abd1f447aff4b5c172e24c3fddf27fb63b7f669
tereneka <teren-eka@yandex.ru>
2022-11-08 14:18:08 +0300

    fix: fix after review2

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
-let editButton = document.querySelector(".profile__edit-btn");
-let popupCloseBtn = document.querySelector(".popup__close-btn");
-let popup = document.querySelector(".popup");
-let editForm = document.querySelector(".popup__form");
-let nameInput = editForm.querySelector(".popup__input_data_name");
-let aboutInput = editForm.querySelector(".popup__input_data_about");
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__about");
+let editButton = document.querySelector('.profile__edit-btn');
+let popupCloseBtn = document.querySelector('.popup__close-btn');
+let popup = document.querySelector('.popup');
+let editForm = document.querySelector('.popup__form');
+let nameInput = editForm.querySelector('.popup__input_data_name');
+let aboutInput = editForm.querySelector('.popup__input_data_about');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 
 function openPopup() {
   nameInput.value = profileName.textContent;
   aboutInput.value = profileAbout.textContent;
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
@@ -26,6 +26,6 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-editButton.addEventListener("click", openPopup);
-popupCloseBtn.addEventListener("click", closePopup);
-editForm.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+editForm.addEventListener('submit', formSubmitHandler);
