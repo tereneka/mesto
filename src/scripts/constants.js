4baa7f3ed9ff262ca7cc9f57f2cb2d2322332dcb
tereneka <teren-eka@yandex.ru>
2023-01-30 13:45:14 +0300

    deploy

--- src/scripts/constants.js
+++
@@ -1,5 +0,0 @@
-// не нашла в задании, что утилитарные константы должны быть выделены в constants.js
-const btnOpenProfileEdit = document.querySelector(".profile__edit-btn");
-const btnOpenCardAdd = document.querySelector(".profile__add-btn");
-
-export { btnOpenProfileEdit, btnOpenCardAdd };
